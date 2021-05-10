878dbced12964abe7e00698544f3bb322d07f5ec
Michael Senn <michael@morrolan.ch>
2021-05-10 15:33:58 +0200

    Add distributed ElGamal

--- elgamal/elgamal.go
+++ elgamal/elgamal.go
@@ -1,10 +1,15 @@
 package elgamal
 
 import (
+	"crypto/sha512"
+	"fmt"
 	"github.com/lavode/secret-sharing/gf"
 	"github.com/lavode/secret-sharing/secretshare"
 	"math/big"
 )
+
+// SHA512
+const hashByteSize int = 64
 
 // PublicKey represents a public key of the ElGamal cryptosystem.
 type PublicKey struct {
@@ -29,6 +34,13 @@ type PrivateKey struct {
 // PrivateKeyShare represents a private key share of the distributed ElGamal
 // cryptosystem.
 type PrivateKeyShare secretshare.Share
+
+type DecryptionShare secretshare.Share
+
+type Ciphertext struct {
+	R *big.Int
+	C []byte
+}
 
 // KeyGen implements key generation for a distributed ElGamal cryptosystem. It
 // is to be executed by a trusted dealer, who can then send out the individual
@@ -55,26 +67,30 @@ func KeyGen(pBits int, qBits int, t int, n int) (PublicKey, PrivateKey, []Privat
 	pub.Q = schnorr.Q
 	pub.G = schnorr.G
 
-	// The private key x is an element of (Z/qZ), such that then g^x is an
-	// element of the subgroup G.
+	// (Z/qZ) is used for:
+	// - Generation of private key x, such that `g^x` is an element of G
+	// - Secret sharing using polynomials over (Z/qZ)
 	zq, err := gf.NewGF(pub.Q)
 	if err != nil {
 		return pub, priv, shares, err
 	}
+
+	// (Z/pZ) is used for all operations *within* G, as it's a subgroup of
+	// (Z/pZ)
+	zp, err := pub.Field()
+	if err != nil {
+		return pub, priv, shares, err
+	}
+
 	x, err := zq.Rand()
 	if err != nil {
 		return pub, priv, shares, err
 	}
 	priv.X = x
 
-	// Public key is g^x mod p
-	zp, err := pub.Field()
-	if err != nil {
-		return pub, priv, shares, err
-	}
 	pub.Y = zp.Exp(pub.G, x)
 
-	tnShares, _, err := secretshare.TOutOfN(priv.X, t, n, zp)
+	tnShares, _, err := secretshare.TOutOfN(priv.X, t, n, zq)
 	if err != nil {
 		return pub, priv, shares, err
 	}
@@ -85,20 +101,99 @@ func KeyGen(pBits int, qBits int, t int, n int) (PublicKey, PrivateKey, []Privat
 	return pub, priv, shares, nil
 }
 
-func Enc(pk PublicKey, message []byte) *big.Int {
-	var ciphertext = &big.Int{}
+func Enc(pub PublicKey, message []byte) (Ciphertext, error) {
+	var ctxt Ciphertext
+	// Using SHA512
+	ctxt.C = make([]byte, hashByteSize)
 
-	return ciphertext
+	if len(message) > hashByteSize {
+		return ctxt, fmt.Errorf("Message must be at most %d bytes; got %d", hashByteSize, len(message))
+	}
+
+	// This will implicitly pad with zero-bytes, if the input message is <hashByteSize byte
+	msg := make([]byte, hashByteSize)
+	copy(msg, message)
+
+	zq, err := gf.NewGF(pub.Q)
+	if err != nil {
+		return ctxt, err
+	}
+	zp, err := pub.Field()
+	if err != nil {
+		return ctxt, err
+	}
+
+	r, err := zq.Rand()
+	if err != nil {
+		return ctxt, err
+	}
+	ctxt.R = zp.Exp(pub.G, r) // g^r = R
+
+	yr := zp.Exp(pub.Y, r) // y^r
+
+	key := sha512.Sum512(yr.Bytes())
+
+	for i, keyByte := range key {
+		ctxt.C[i] = msg[i] ^ keyByte
+	}
+
+	return ctxt, nil
 }
 
-func Dec(pk PublicKey, sk PrivateKeyShare, ciphertext *big.Int) *big.Int {
-	var decryptionShare = &big.Int{}
+func Dec(pub PublicKey, keyShare PrivateKeyShare, ctxt Ciphertext) (DecryptionShare, error) {
+	decryptionShare := DecryptionShare(
+		secretshare.Share{
+			ID: keyShare.ID,
+		},
+	)
 
-	return decryptionShare
+	zp, err := pub.Field()
+	if err != nil {
+		return decryptionShare, err
+	}
+
+	// Secret sharing happens over (Z/pZ)
+	decryptionShare.Value = zp.Exp(ctxt.R, keyShare.Value) // R^{x_i} mod p
+
+	return decryptionShare, nil
 }
 
-func Recover(pk PublicKey, decryptionShares []*big.Int, ciphertext *big.Int) *big.Int {
-	var msg = &big.Int{}
+func Recover(pub PublicKey, decryptionShares []DecryptionShare, ctxt Ciphertext) ([]byte, error) {
+	msg := make([]byte, hashByteSize)
 
-	return msg
+	xs := make([]*big.Int, len(decryptionShares))
+	for i, share := range decryptionShares {
+		xs[i] = big.NewInt(int64(share.ID))
+	}
+
+	zp, err := pub.Field()
+	if err != nil {
+		return msg, err
+	}
+
+	// Mind that secret sharing happens over (Z/qZ)
+	zq, err := gf.NewGF(pub.Q)
+	if err != nil {
+		return msg, err
+	}
+
+	// Starting with 1, as identity of multiplication
+	z := big.NewInt(1)
+
+	for i, share := range decryptionShares {
+		// Polynomial's coefficients (and such also lagrange
+		// coefficients) are over (Z/qZ)
+		bp := gf.BasePolynomial(i, xs, zq)
+		// But the value we reconstruct is in G, so we operate over (Z/pZ)
+		factor := zp.Exp(share.Value, bp)
+		z = zp.Mul(z, factor)
+	}
+
+	key := sha512.Sum512(z.Bytes())
+
+	for i, keyByte := range key {
+		msg[i] = ctxt.C[i] ^ keyByte
+	}
+
+	return msg, nil
 }
--- elgamal/elgamal_test.go
+++ elgamal/elgamal_test.go
@@ -1,6 +1,8 @@
 package elgamal
 
 import (
+	"bytes"
+	"github.com/lavode/secret-sharing/gf"
 	"github.com/lavode/secret-sharing/secretshare"
 	"math/big"
 	"testing"
@@ -53,7 +55,7 @@ func TestKeyGen(t *testing.T) {
 		t.Errorf("Expected 5 shares; got %d", len(shares))
 	}
 
-	field, err := pub.Field()
+	field, err := gf.NewGF(pub.Q)
 	if err != nil {
 		t.Fatalf("Error generating field: %v", err)
 	}
@@ -69,5 +71,182 @@ func TestKeyGen(t *testing.T) {
 	}
 	if recoveredSecret.Cmp(priv.X) != 0 {
 		t.Errorf("Expected recovered secret = %d; got %d", priv.X, recoveredSecret)
+	}
+}
+
+func TestEnc(t *testing.T) {
+	pub := PublicKey{
+		SchnorrGroup: SchnorrGroup{
+			P: big.NewInt(23),
+			Q: big.NewInt(11),
+			G: big.NewInt(4),
+		},
+		Y: big.NewInt(8),
+	}
+
+	msg := []byte("Hello world")
+	ctxt, err := Enc(pub, msg)
+	if err != nil {
+		t.Fatalf("Enc returned error: %v", err)
+	}
+	if len(ctxt.C) != 64 {
+		t.Errorf("Expected ciphertext of 64 bytes; got %d", len(ctxt.C))
+	}
+	// TODO test that decryption returns actual message
+	// TODO test that only correct-length message is allowed
+
+	msg = make([]byte, 65)
+	_, err = Enc(pub, msg)
+	if err == nil {
+		t.Errorf("Expected error if message exceeds 64 bytes; got none")
+	}
+}
+
+func TestDec(t *testing.T) {
+	pub := PublicKey{
+		SchnorrGroup: SchnorrGroup{
+			P: big.NewInt(23),
+			Q: big.NewInt(11),
+			G: big.NewInt(4),
+		},
+		Y: big.NewInt(16), // x = 2
+	}
+
+	// Ciphertext encoding of message 'Hello world' padded to 64 bytes
+	// Only the `R` component is relevant for this test
+	ctxt := Ciphertext{
+		R: big.NewInt(3), // r = 4
+		C: []byte{0xBA, 0x1E, 0x37, 0x94, 0xBC, 0x7E, 0xD5, 0xD4, 0xC9, 0x0, 0x6B, 0x9F, 0xEF, 0x89, 0xD8, 0x83, 0x41, 0x5B, 0x5A, 0xDB, 0xD6, 0xA8, 0x40, 0x30, 0xCB, 0x1F, 0x35, 0xE6, 0xA6, 0xC0, 0x26, 0xE6, 0x5C, 0x60, 0xFB, 0x99, 0xF5, 0x62, 0xF7, 0xEB, 0x9F, 0x77, 0xF3, 0xDE, 0xC5, 0x0, 0x14, 0x73, 0x44, 0x1D, 0x2C, 0x55, 0x86, 0xB5, 0x4D, 0x9B, 0x99, 0x9C, 0xF4, 0xBD, 0x79, 0xE, 0x4C, 0x56},
+	}
+
+	// Three keyshares out of a 3-out-of-5 secret share of x and their
+	// corresponding (expected) decryption shares.
+	k1 := PrivateKeyShare(secretshare.Share{ID: 1, Value: big.NewInt(4)})
+	k3 := PrivateKeyShare(secretshare.Share{ID: 3, Value: big.NewInt(14)})
+	k4 := PrivateKeyShare(secretshare.Share{ID: 4, Value: big.NewInt(22)})
+
+	ed1 := DecryptionShare(secretshare.Share{ID: 1, Value: big.NewInt(12)})
+	ed3 := DecryptionShare(secretshare.Share{ID: 3, Value: big.NewInt(4)})
+	ed4 := DecryptionShare(secretshare.Share{ID: 4, Value: big.NewInt(1)})
+
+	d1, err := Dec(pub, k1, ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+	if d1.ID != ed1.ID || d1.Value.Cmp(ed1.Value) != 0 {
+		t.Errorf("Expected decryption share %+v; got %+v", ed1, d1)
+	}
+
+	d3, err := Dec(pub, k3, ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+	if d3.ID != ed3.ID || d3.Value.Cmp(ed3.Value) != 0 {
+		t.Errorf("Expected decryption share %+v; got %+v", ed3, d3)
+	}
+
+	d4, err := Dec(pub, k4, ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+	if d4.ID != ed4.ID || d4.Value.Cmp(ed4.Value) != 0 {
+		t.Errorf("Expected decryption share %+v; got %+v", ed4, d4)
+	}
+}
+
+func TestRecover(t *testing.T) {
+	// 'Hello world', padded to 64 bytes
+	msg := []byte{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+
+	pub := PublicKey{
+		SchnorrGroup: SchnorrGroup{
+			P: big.NewInt(23),
+			Q: big.NewInt(11),
+			G: big.NewInt(4),
+		},
+		Y: big.NewInt(16), // x = 2
+	}
+
+	// // Three keyshares out of a 3-out-of-5 secret share of x
+	// keyShares := []PrivateKeyShare{
+	// 	PrivateKeyShare(secretshare.Share{ID: 1, Value: 4}}),
+	// 	PrivateKeyShare(secretshare.Share{ID: 3, Value: 14}}),
+	// 	PrivateKeyShare(secretshare.Share{ID: 4, Value: 22}}),
+	// }
+
+	// Ciphertext encoding of message above
+	ctxt := Ciphertext{
+		R: big.NewInt(3), // r = 4
+		C: []byte{0xBA, 0x1E, 0x37, 0x94, 0xBC, 0x7E, 0xD5, 0xD4, 0xC9, 0x0, 0x6B, 0x9F, 0xEF, 0x89, 0xD8, 0x83, 0x41, 0x5B, 0x5A, 0xDB, 0xD6, 0xA8, 0x40, 0x30, 0xCB, 0x1F, 0x35, 0xE6, 0xA6, 0xC0, 0x26, 0xE6, 0x5C, 0x60, 0xFB, 0x99, 0xF5, 0x62, 0xF7, 0xEB, 0x9F, 0x77, 0xF3, 0xDE, 0xC5, 0x0, 0x14, 0x73, 0x44, 0x1D, 0x2C, 0x55, 0x86, 0xB5, 0x4D, 0x9B, 0x99, 0x9C, 0xF4, 0xBD, 0x79, 0xE, 0x4C, 0x56},
+	}
+
+	// 3-out-of-5 decryption shares of ciphertext above
+	decryptionShares := []DecryptionShare{
+		DecryptionShare(secretshare.Share{ID: 1, Value: big.NewInt(4)}),
+		DecryptionShare(secretshare.Share{ID: 3, Value: big.NewInt(4)}),
+		DecryptionShare(secretshare.Share{ID: 4, Value: big.NewInt(9)}),
+	}
+
+	recovered, err := Recover(pub, decryptionShares, ctxt)
+	if err != nil {
+		t.Fatalf("Recover returned error: %v", err)
+	}
+
+	if !bytes.Equal(recovered, msg) {
+		t.Errorf("Recovered message did not match actual message; got %x; expected %x", recovered, msg)
+	}
+
+}
+
+// This tests the whole thing end-to-end, with real-world keys.
+// Hopefully catching any issues which might be the result of the
+// handcrafted values above.
+func TestIntegration(t *testing.T) {
+	// 'Hello world', padded to 64 bytes
+	msg := []byte{0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0}
+
+	pub, _, privShares, err := KeyGen(1024, 256, 4, 6)
+	if err != nil {
+		t.Fatalf("KeyGen returned error: %v", err)
+	}
+
+	ctxt, err := Enc(pub, msg)
+	if err != nil {
+		t.Fatalf("Enc returned error: %v", err)
+	}
+
+	decShare1, err := Dec(pub, privShares[0], ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+
+	decShare3, err := Dec(pub, privShares[2], ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+
+	decShare4, err := Dec(pub, privShares[3], ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+
+	decShare5, err := Dec(pub, privShares[4], ctxt)
+	if err != nil {
+		t.Fatalf("Dec returned error: %v", err)
+	}
+
+	decShares := []DecryptionShare{
+		decShare1,
+		decShare3,
+		decShare4,
+		decShare5,
+	}
+
+	recov, err := Recover(pub, decShares, ctxt)
+	if err != nil {
+		t.Fatalf("Recover returned error: %v", err)
+	}
+	if !bytes.Equal(msg, recov) {
+		t.Errorf("Expected recovered message %x; got %x", msg, recov)
 	}
 }
